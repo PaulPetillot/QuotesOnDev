a6a8adf38ec98b30877416560bf713e0205b161d
Paul Petillot <petillotpaul@gmail.com>
2018-11-29 14:09:59 +0000

    ES-Lint fixing and history-api  adding

--- js/api.js
+++ js/api.js
@@ -2,65 +2,80 @@
 
     /* Ajax-based random post fetching. */
     $(function () {
-        $(".new-quote-button").on('click', function (event) {
+        let lastPage = '';
+        $('.new-quote-button').on('click', function (event) {
             event.preventDefault();
-            $description = $('.description');
-            $metah2 = $('.metah2');
-            $authorName = $('.author-name');
+
+            lastPage = document.URL;
+           let $description = $('.description');
+           let $metah2 = $('.metah2');
+           let $authorName = $('.author-name');
             // fetch a new quote
             $.ajax({
                 method: 'GET',
-                url: api_vars.root_url + 'wp/v2/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+                url: api_vars.root_url + 'wp/v2/posts?filter[orderby]=rand&filter[posts_per_page]=1',// eslint-disable-line
                 cache: false
             }).done(function (data) {
-                const post = data.shift();
+                const post = data.shift(),
                 quoteSource = post._qod_quote_source,
-                    quoteSourceUrl = post._qod_quote_source_url,
-                    the_title = post.title.rendered,
-                    the_excerpt = post.excerpt.rendered;
-                //update the quote content and name of the quoted peson
-                //maybe use appemnd
-                $description.html(the_excerpt);
-                //$('.link-source-url').html(quoteSourceUrl)
+                quoteSourceUrl = post._qod_quote_source_url,
+                theTitle = post.title.rendered, 
+                theExcerpt = post.excerpt.rendered, 
+                postLink = post.link;
+                $description.html(theExcerpt);
+              //  $('.link-source-url').html(quoteSourceUrl)
+                if (quoteSource.length>0 && quoteSourceUrl.length>0){
                 $metah2.html('<a href="' + quoteSourceUrl + '">' + quoteSource + '</a>');
-                $authorName.html(the_title);
+                }else if (quoteSource.lenght>0){
+                    $metah2.html('<a href="">' + quoteSource + '</a>');
+                }
+                else{
+                    $metah2.html('<a href=""></a>');
+                }
+                $authorName.html(theTitle);
                 //update the URL using history
+               history.pushState(null, null, postLink);
             });
         })
-
-        // Make the back and forward nav work with the history API
+         //Make the back and forward nav work with the history API
+        $(window).on('popstate', function(){
+            window.location.replace(lastPage);
+        });
     });
 
     /* Ajax-based front-end post submissions */
     $(function () {
-        $('.submit-form').on('submit', function (event) {
+        let $submitForm = $('.submit-form')
+        $submitForm.on('submit', function (event) {
         event.preventDefault();
         // Event on submit of the form
+        let $messageReceive = $('.message-receive')
+        $messageReceive.empty();
         let data = {
             title: $('#author-name-field').val(),
             content: $('#quote-content-field').val(),
-            _qod_quote_source: $('#quote-source-field').val(),
-            _qod_quote_source_url: $('#quote-url-field').val(),
-            post_status: 'pending'
+            _qod_quote_source: $('#quote-source-field').val(),// eslint-disable-line
+            _qod_quote_source_url: $('#quote-url-field').val(),// eslint-disable-line
+            post_status: 'pending'// eslint-disable-line
         };
             $.ajax({
                 method: 'POST',
-                url: api_vars.root_url + 'wp/v2/posts',
+                url: api_vars.root_url + 'wp/v2/posts',// eslint-disable-line
                 data:data,
                 beforeSend: function (xhr) {
-                    xhr.setRequestHeader('X-WP-Nonce', api_vars.nonce);
+                    xhr.setRequestHeader('X-WP-Nonce', api_vars.nonce);// eslint-disable-line
                 }
 
             }).done(function () {
                 //Use jquey so hide the form in a slidey way
                 event.preventDefault();
-                $('.submit-form').css('display', 'none');
-                $('.message-receive').append('Thanks, your quote submission was received !')
+                $submitForm.css('display', 'none');
+                $messageReceive.text(api_vars.success);// eslint-disable-line
                 //show success message using the var from functions.php
 
 
             }).fail(function () {
-                // post and alert wih failure var from functions.php
+                $messageReceive.text(api_vars.failure);// eslint-disable-line
             })
         });
     });
